9a1f588c3d3767e047a0fb5e341a4dc68f790204
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2018-12-11 18:57:14 +0100

    ♻️ externalize routes in data files

--- data/capacities.js
+++ data/capacities.js
@@ -1,11 +1,91 @@
 const Joi = require('joi');
 
-exports.schema = Joi.object({
+const schema = Joi.object({
     id: Joi.number(),
     label: Joi.string().required(),
 });
 
-exports.getCapacities = () => [
+const getRoutes = (db) => [{
+    method: 'GET',
+    path: '/capacities',
+    handler: function (request, reply) {
+        return reply(db.capacities);
+    },
+    config: {
+        tags: ['api'],
+        response: {
+            schema: Joi.array().items(schema)
+        }
+    }
+}, {
+    method: 'GET',
+    path: '/capacities/{capacityId}',
+    handler: function (request, reply) {
+        const capacity = _(db.capacities).find({id: +request.params.capacityId});
+        if (capacity) {
+            return reply(capacity);
+        } else {
+            return reply(`Capacity '${request.params.capacityId}' not found`).code(404);
+        }
+    },
+    config: {
+        tags: ['api'],
+        response: {
+            schema: schema
+        }
+    }
+}, {
+    method: 'POST',
+    path: '/capacities',
+    handler: function (request, reply) {
+        let newCapacity = request.payload;
+        newCapacity.id = (_(db.capacities).map('id').max() || 0) + 1;
+        db.capacities.push(newCapacity);
+        return reply(newCapacity).code(201);
+    },
+    config: {
+        tags: ['api'],
+        validate: {
+            payload: schema
+        }
+    }
+}, {
+    method: 'PUT',
+    path: '/capacities/{capacityId}',
+    handler: function (request, reply) {
+        const updatedCapacity = request.payload;
+        if (updatedCapacity.id !== +request.params.capacityId) {
+            return reply('Incoherent capacity ID between request param and payload').code(500);
+        }
+        if (!_(db.capacities).find({id: updatedCapacity.id})) {
+            return reply(`Capacity '${request.params.capacityId}' not found`).code(404);
+        }
+        db.capacities = _(db.capacities).filter((capacity) => capacity.id !== updatedCapacity.id).value();
+        db.capacities.push(updatedCapacity);
+        return reply(updatedCapacity);
+    },
+    config: {
+        tags: ['api'],
+        validate: {
+            payload: schema
+        }
+    }
+}, {
+    method: 'DELETE',
+    path: '/capacities/{capacityId}',
+    handler: function (request, reply) {
+        if (!_(db.capacities).find({id: +request.params.capacityId})) {
+            return reply(`Capacity '${request.params.capacityId}' not found`).code(404);
+        }
+        db.capacities = _(db.capacities).filter((capacity) => capacity.id !== +request.params.capacityId).value();
+        return reply();
+    },
+    config: {
+        tags: ['api']
+    }
+}];
+
+const getCapacities = () => [
     {
         id: 1,
         label: 'Strong'
@@ -23,3 +103,5 @@ exports.getCapacities = () => [
         label: 'Telepath'
     }
 ];
+
+module.exports = {getRoutes, getCapacities, schema};
--- data/unicorns.js
+++ data/unicorns.js
@@ -1,6 +1,6 @@
 const Joi = require('joi');
 
-exports.schema = Joi.object({
+const schema = Joi.object({
     id: Joi.number(),
     name: Joi.string().required(),
     birthyear: Joi.number().integer().min(1900).max(new Date().getFullYear()),
@@ -10,7 +10,98 @@ exports.schema = Joi.object({
     capacities: Joi.array().items(Joi.number()).unique()
 });
 
-exports.getUnicorns = (server) => {
+const getRoutes = (db) => [{
+    method: 'GET',
+    path: '/unicorns',
+    handler: function (request, reply) {
+        return reply(db.unicorns);
+    },
+    config: {
+        tags: ['api'],
+        response: {
+            schema: Joi.array().items(schema)
+        }
+    }
+}, {
+    method: 'GET',
+    path: '/unicorns/{unicornId}',
+    handler: function (request, reply) {
+        const unicorn = _(db.unicorns).find({id: +request.params.unicornId});
+        if (unicorn) {
+            return reply(unicorn);
+        } else {
+            return reply(`Unicorn '${request.params.unicornId}' not found`).code(404);
+        }
+    },
+    config: {
+        tags: ['api'],
+        response: {
+            schema: schema
+        }
+    }
+}, {
+    // Add unicorns photos
+    method: 'GET',
+    path: '/unicorns/{param*}',
+    handler: {
+        directory: {
+            path: '.',
+            redirectToSlash: true,
+            index: true,
+        }
+    }
+}, {
+    method: 'POST',
+    path: '/unicorns',
+    handler: function (request, reply) {
+        let newUnicorn = request.payload;
+        newUnicorn.id = (_(db.unicorns).map('id').max() || 0) + 1;
+        db.unicorns.push(newUnicorn);
+        return reply(newUnicorn).code(201);
+    },
+    config: {
+        tags: ['api'],
+        validate: {
+            payload: schema
+        }
+    }
+}, {
+    method: 'PUT',
+    path: '/unicorns/{unicornId}',
+    handler: function (request, reply) {
+        const updatedUnicorn = request.payload;
+        if (updatedUnicorn.id !== +request.params.unicornId) {
+            return reply('Incoherent unicorn ID between request param and payload').code(500);
+        }
+        if (!_(db.unicorns).find({id: updatedUnicorn.id})) {
+            return reply(`Unicorn '${request.params.unicornId}' not found`).code(404);
+        }
+        db.unicorns = _(db.unicorns).filter((unicorn) => unicorn.id !== updatedUnicorn.id).value();
+        db.unicorns.push(updatedUnicorn);
+        return reply(updatedUnicorn);
+    },
+    config: {
+        tags: ['api'],
+        validate: {
+            payload: schema
+        }
+    }
+}, {
+    method: 'DELETE',
+    path: '/unicorns/{unicornId}',
+    handler: function (request, reply) {
+        if (!_(db.unicorns).find({id: +request.params.unicornId})) {
+            return reply(`Unicorn '${request.params.unicornId}' not found`).code(404);
+        }
+        db.unicorns = _(db.unicorns).filter((unicorn) => unicorn.id !== +request.params.unicornId).value();
+        return reply();
+    },
+    config: {
+        tags: ['api']
+    }
+}];
+
+const getUnicorns = (server) => {
     return [
         {
             id: 1,
@@ -69,3 +160,5 @@ exports.getUnicorns = (server) => {
         }
     ];
 };
+
+module.exports = {getRoutes, getUnicorns, schema};
